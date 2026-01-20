5a633e59bfc17497ec9d57cae7b9a9d3eead8749
rprat-pro <rp269144@pleiades1217.intra.cea.fr>
2026-01-20 21:04:56 +0100

    try

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Raphaël Prat, Thierry Carrard, Paul Lafourcade'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme', 'sphinxcontrib.bibtex', 'myst_parser','sphinx_design']
+extensions = ['sphinx_rtd_theme', 'sphinxcontrib.bibtex', 'myst_parser','sphinx_design','sphinx_tabs.tabs']
 
 bibtex_bibfiles= ["../doc_exaNBody/sources/bibliography.bib"]
 
